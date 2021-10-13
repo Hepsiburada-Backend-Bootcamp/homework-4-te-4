34166f6eb8a652b8d1d48a92a3993ade55c35034
Mahmud Esad Ünal <m@halukunal.com>
2021-10-14 00:13:41 +0300

    Created Dtos for product

---
+++ Ecommerce/Ecommerce.Application/Dtos/DtoMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Ecommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Dtos
+{
+    class DtoMapping : Profile
+    {
+        public DtoMapping()
+        {
+            CreateMap<Product, ProductDto>();
+            CreateMap<CreateProductDto, Product>();
+            CreateMap<UpdateProductDto, Product>();
+        }
+    }
+}
