bf475af119cff9a2205e331746a752d4e1d3a3fb
alprsln <alpars44@hotmail.com>
2021-10-15 02:54:48 +0300

    Add IOrderRepository

---
+++ Ecommerce/Ecommerce.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ecommerce.Domain.Models;
+
+namespace Ecommerce.Domain.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<Guid> AddAsync(Order order);
+        Task<List<Order>> GetAllAsync();
+        Task<Order> FindByIdAsync(Guid orderId);
+        Task DeleteAsync(Guid orderId);
+        Task<Order> FindByUserIdAsync(Guid userId);
+        
+        //No adding or subtracting
+        Task <bool> CreateOrderItem(OrderItem orderItem);
+        Task <bool> DeleteOrderItem(Guid orderItemId);
+        
+        //Check if orderItem exists, update quantity
+        Task<bool> UpdateOrderItemQuantity(Guid orderItemId, int quantity);
+        Task<bool> FinalizeOrder(Guid orderId);
+
+    }
+}
