a63ab1dd51522e57055fadb2fc97ba5137d94585
Çağlar Demir <cagdem2@gmail.com>
2021-10-15 00:27:17 +0300

    User service added

---
+++ Ecommerce/Ecommerce.Application/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Services
+{
+    public interface IUserService
+    {
+        Task<Guid> CreateUser(CreateUserDto dto);
+        Task<List<UserDto>> GetUsers();
+        Task<UserDto> GetUser(Guid id);
+        Task DeleteUser(Guid id);
+        Task UpdateUser(Guid id, UpdateUserDto dto);
+    }
+}
