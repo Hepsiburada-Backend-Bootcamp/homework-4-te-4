04d931219d1b5a484f7e62f1a831204d70886ead
alprsln <alpars44@hotmail.com>
2021-10-17 02:19:01 +0300

    add order controller

---
+++ Ecommerce/Ecommerce.API/Controllers/OrderController.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Threading.Tasks;
+using Ecommerce.Application.Dtos;
+using Ecommerce.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/orders")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _service;
+
+        public OrderController(IOrderService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrder([FromBody] CreateOrderDto dto)
+        {
+            return Ok(await _service.CreateOrder(dto));
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteOrder([FromRoute]Guid id)
+        {
+            await _service.DeleteOrder(id);
+            return Ok();
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetOrder([FromRoute]Guid id)
+        {
+            return Ok(await _service.GetOrder(id));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders()
+        {
+            return Ok(await _service.GetOrders());
+        }
+        
+        [HttpGet("user/{userId:guid}")]
+        public async Task<IActionResult> GetOrdersOfUser([FromRoute]Guid userId)
+        {
+            return Ok(await _service.GetOrdersOfUser(userId));
+        }
+
+        [HttpPost("{orderId:guid}")]
+        public async Task<IActionResult> AddItemToOrder([FromRoute]Guid orderId, [FromBody] CreateOrderItemDto dto)
+        {
+            if(orderId == dto.OrderId)
+                return Ok(await _service.AddOrderItem(dto));
+            return BadRequest();
+        }
+        
+        [HttpDelete("{orderId:guid}/{orderItemId:guid}")]
+        public async Task<IActionResult> RemoveItemFromOrder([FromRoute]Guid orderId, [FromRoute]Guid orderItemId)
+        {
+            return Ok(await _service.RemoveOrderItem(orderId,orderItemId));
+        }
+
+        [HttpPatch("{orderId:guid}/{orderItemId:guid}")]
+        public async Task<IActionResult> UpdateItemQuantity([FromRoute]Guid orderId, [FromRoute]Guid orderItemId, [FromQuery]int quantity)
+        {
+            //TODO: validate orderId
+            return Ok(await _service.UpdateOrderItemQuantity(orderItemId,quantity));
+        }
+
+        [HttpPatch("{orderId:guid}")]
+        public async Task<IActionResult> FinalizeOrder([FromRoute]Guid orderId)
+        {
+            return Ok(await _service.FinalizeOrder(orderId));
+        }
+    }
+}
