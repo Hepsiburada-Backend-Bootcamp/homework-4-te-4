f440b34822e066540ff1bb038b901c36257cdcd2
Mahmud Esad Ünal <m@halukunal.com>
2021-10-17 00:40:51 +0300

    Implemented Order Dtos

---
+++ Ecommerce/Ecommerce.Application/Dtos/CreateOrderDto.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Dtos
+{
+    class CreateOrderDto
+    {
+        [Required]
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Ecommerce/Ecommerce.Application/Dtos/CreateOrderItemDto.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Application.Dtos
+{
+    class CreateOrderItemDto
+    {
+        [Required]
+        public Guid OrderId { get; set; }
+        [Required]
+        public Guid ProductId { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public int Quantity { get; set; }
+    }
+}
