f334bbf6c16705a4d4091ec01e368363e706fece
Mahmud Esad Ünal <m@halukunal.com>
2021-10-17 00:41:29 +0300

    Added Order Service

---
+++ Ecommerce/Ecommerce.Application/Services/IOrderService.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Services
+{
+    interface IOrderService
+    {
+        Task<Guid> CreateOrder(CreateOrderDto dto);
+        Task<List<OrderDto>> GetOrders();
+        Task<OrderDto> GetOrder(Guid id);
+        Task DeleteOrder(Guid id);
+        Task<List<OrderDto>> GetOrdersOfUser(Guid userId);
+
+        Task<bool> AddOrderItem(CreateOrderItemDto dto);
+        Task<bool> RemoveOrderItem(Guid orderItemId);
+
+        Task<bool> UpdateOrderItemQuantity(Guid orderItemId, int quantity);
+        Task<bool> FinalizeOrder(Guid orderId);
+
+    }
+}
